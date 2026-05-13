e795cabb51d4cee68c1e8e723f930de8fee7617b
Yoshika02 <56820990+yoshika02@users.noreply.github.com>
2026-05-14 00:28:27 +0530

    working

--- biolab-ai/next.config.ts
+++ biolab-ai/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  outputFileTracingRoot: process.cwd(),
 };
 
 export default nextConfig;
